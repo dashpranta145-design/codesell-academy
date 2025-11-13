5b64fbeefe09db8b9ae61f501bf089953326bf0a
Sanjit Dash <dash.pranta145@gmail.com>
2025-11-13 20:40:20 +0600

    Teampage updated

--- src/components/TeamPage.tsx
+++ src/components/TeamPage.tsx
@@ -225,7 +225,7 @@ const TeamPage: React.FC = () => {
                     </div>
                     <div className="bg-primary/10 border border-primary/30 rounded-xl p-4 text-center">
                       <div className="text-3xl font-bold text-primary mb-2">
-                        100+
+                        50+
                       </div>
                       <div className="text-sm text-gray-400">
                         Students Mentored
