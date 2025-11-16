5ec6ebb22810b26fe2addace04c19c6b31111522
Sanjit Dash <dash.pranta145@gmail.com>
2025-11-16 16:08:46 +0600

    Gap is reduced

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -66,8 +66,8 @@ const Header: React.FC = () => {
       className="bg-black/80 backdrop-blur-sm text-white p-4 lg:px-8 fixed top-0 left-0 right-0 z-50 shadow-lg shadow-primary/10"
     >
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center gap-4">
-          <div className="w-16 h-16 flex items-center justify-center ml-10">
+        <div className="flex items-center gap-2">
+          <div className="w-16 h-16 flex items-center justify-center">
             <img
               src="/images/CodeSell Logo.png"
               alt="CodeSell Academy Logo"
